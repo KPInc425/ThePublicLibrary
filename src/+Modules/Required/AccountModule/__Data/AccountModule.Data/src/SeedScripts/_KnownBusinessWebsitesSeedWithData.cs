bd89238cb6695e6591e92b1ebc50b895d029e1bf
JamesK <James@10-geek.com>
2023-07-06 15:24:53 -0500

    Continued working on automated tests in Account Module

---
+++ src/+Modules/Required/AccountModule/__Data/AccountModule.Data/src/SeedScripts/_KnownBusinessWebsitesSeedWithData.cs
@@ -0,0 +1,29 @@
+namespace AccountModule.Data.SeedScripts;
+public class KnownBusinessWebsitesSeedWithData : IAccountModuleSeedScript
+{
+    public async Task PopulateAccountModuleTestData(IServiceProvider serviceProvider)
+    {
+        /* var mediator = serviceProvider.GetRequiredService<IMediator>();
+        var logger = serviceProvider.GetRequiredService<ILogger<BooksSeedWithData>>();
+
+        using var dbContext =
+                new KnownAccountDbContext(serviceProvider
+                        .GetRequiredService<DbContextOptions<KnownAccountDbContext>>(
+                        ), mediator);
+
+       
+        if (!dbContext.Libraries.Any())
+        {
+            dbContext.Libraries.AddRange(LibraryAccountModuleTestData.AllLibraries);
+            await dbContext.SaveChangesAsync();
+            logger?.LogInformation("Seeded All Library Data");
+        }
+
+        if (dbContext.Libraries.FirstOrDefault(rs => rs.Name == LibraryAccountModuleTestData.FirstStreetLibraryName) is null)
+        {
+            dbContext.Libraries.Add(LibraryAccountModuleTestData.FirstStreetLibrary);
+            await dbContext.SaveChangesAsync();
+            logger?.LogInformation("Seeded Single Library Data");
+        } */
+    }
+}
